a975058d673994313589af18b6777f9bf7092461
Yu991484884 <hokuohyuha@gmail.com>
2024-12-20 16:08:17 +0900

    Updated LoginView.vue with new features/bug fixes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require("path");
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -7,5 +8,15 @@ module.exports = defineConfig({
     port: 8080, // サーバーのポートを指定（必要に応じて変更）
     open: false, // 自動でブラウザを開かない（必要に応じてtrueに変更可能）
     allowedHosts: "all", // 他のホストからのアクセスを許可
+  },
+  chainWebpack: (config) => {
+    config.plugin("copy").tap(([options]) => {
+      options.patterns.push({
+        from: path.resolve(__dirname, "_redirects"), // プロジェクトのルートから_redirectsを取得
+        to: path.resolve(__dirname, "dist"), // distフォルダにコピー
+        toType: "dir",
+      });
+      return [options];
+    });
   },
 });
